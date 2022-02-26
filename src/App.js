3db6e2cfcacb69ed3157c2a99f76ea360a4f0159
Duwan <yucsimigliced14@gmail.com>
2022-02-25 19:00:43 -0800

    noticias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import Header from './Componentes/Header';
+import Formulario from './Componentes/Formulario';
+import ListadoNoticias from './Componentes/ListadiNoticias';
 function App() {
+
+  //definir la categoria y noticias
+
+  const [categoria, guardarCategoria] = useState('')
+  const [noticias, guardarNoticia] = useState([])
+
+  useEffect(() => {
+
+    const consultarApi = async () => {
+
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=0878f7edeb4d458888131de7bcfd0b82`
+      const respuesta = await fetch(url)
+      const noticias = await respuesta.json()
+
+      guardarNoticia(noticias.articles)
+    }
+    consultarApi()
+  }, [categoria])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div>
+
+        <Header
+          titulo='Buscador de Noticias'
+        />
+
+      </div>
+      <div className='container white'>
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
+        <ListadoNoticias
+          noticias={noticias}
+        />
+      </div>
+
+    </div >
   );
 }
 
---
+++ src/Componentes/ListadiNoticias.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Noticia from './Noticias';
+const ListadoNoticias = ({ noticias }) => {
+    return (
+        <div className='row '>
+            {noticias.map(noticia => (
+                <Noticia
+                    key={noticia.url}
+                    noticia={noticia}
+                />
+
+            ))}
+
+        </div>
+
+    );
+}
+
+export default ListadoNoticias;
